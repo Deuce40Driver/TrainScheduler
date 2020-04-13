8d46ccadb18283bc534fd8d06802c8faa3e64dbd
Brian L. Thornton <thorntonbl1@gmail.com>
2020-04-12 23:45:03 -0400

    App Rebuild

--- assets/logic.js
+++ assets/logic.js
@@ -1,72 +1,74 @@
-<script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
-
-var config = {
+// Firebase configuration
+var firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "train-scheduler-2d39f.firebaseapp.com",
   databaseURL: "https://train-scheduler-2d39f.firebaseio.com",
   projectId: "train-scheduler-2d39f",
   storageBucket: "train-scheduler-2d39f.appspot.com",
-  messagingSenderId: "2769953834"
+  messagingSenderId: "2769953834",
+  appId: "1:2769953834:web:547cf473a6f0eb00148f53"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
+// Add New Train Button
+$("#add-train-btn").on("click", function(event) {
+  event.preventDefault();
+
+  // Assign user inputs
+  var trainName = $("#train-name-input").val().trim();
+  var trainDestination = $("#destination-input").val().trim();
+  var trainStart = moment($("#start-input").val().trim(), "HH:mm").format("HH:mm");
+  var trainRate = $("#rate-input").val().trim();
+
+  // Create new newTrain object
+  var newTrain = {
+    name: trainName,
+    destination: trainDestination,
+    start: trainStart,
+    rate: trainRate
   };
-  
-  firebase.initializeApp(config);
-  
-  var database = firebase.database();
-  
-  $("#add-train-btn").on("click", function(event) {
-    event.preventDefault();
-  
-    var trainName = $("#train-name-input").val().trim();
-    var trainDestination = $("#destination-input").val().trim();
-    var trainStart = moment($("#start-input").val().trim(), "HH:mm").format("X");
-    var trainRate = $("#rate-input").val().trim();
-  
-    var newTrain = {
-      name: trainName,
-      destination: trainDestination,
-      start: trainStart,
-      rate: trainRate
-    };
-  
-    database.ref().push(newTrain);
-  
-    console.log(newTrain.name);
-    console.log(newTrain.destination);
-    console.log(newTrain.start);
-    console.log(newTrain.rate);
-  
-    alert("Train Successfully Added");
-  
-    $("#train-name-input").val("");
-    $("#destination-input").val("");
-    $("#start-input").val("");
-    $("#rate-input").val("");
-  });
-  
-  database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-  
-    console.log(childSnapshot.val());
-  
-    var trainName = childSnapshot.val().name;
-    var trainDestination = childSnapshot.val().destination;
-    var trainStart = childSnapshot.val().start;
-    var trainRate = childSnapshot.val().rate;
-  
-    console.log(trainName);
-    console.log(trainDestination);
-    console.log(trainStart);
-    console.log(trainRate);
 
-    var trainStartPretty = moment.unix(trainStart).format("HH:mm");
-  
-    var trainArrival = moment().diff(moment(trainStart, "X"), "minutes");
-    console.log(trainArrival);
+  // Push new train to Firebase
+  database.ref().push(newTrain);
+  console.log("New Train Added...")
+  console.log(newTrain);
 
-    var minAway = trainArrival * trainRate;
-    console.log(empBilled);
-  
-    $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" +
-    trainStartPretty + "</td><td>" + trainMonths + "</td><td>" + trainRate + "</td></tr>");
-  });
+  // Clear Modal input and hide dropdown
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#start-input").val("");
+  $("#rate-input").val("");
+  $('.dropdown-toggle').dropdown('hide');
+  $('#exampleModal').modal('show');
+});
 
-  
+database.ref().on("child_added", function(childSnapshot) {
+  console.log(childSnapshot.val());
+
+  var trainName = childSnapshot.val().name;
+  var trainDestination = childSnapshot.val().destination;
+  var trainStart = childSnapshot.val().start
+  var trainRate = parseInt(childSnapshot.val().rate);
+  var startTime = moment(trainStart, 'HH:mm');
+  var minAway = 0;
+  var nextArrival = 0;
+  var currentTime = moment();
+
+  if (moment().isBefore(startTime)) {
+    minAway = (startTime.diff(currentTime, 'minutes'));
+    nextArrival = moment(startTime).format("h:mm a");
+    console.log("First train is " + minAway + " minutes from now at " + nextArrival)
+  };
+
+  if (moment().isAfter(startTime)) {
+    minAway = trainRate - ((currentTime.diff(startTime, 'minutes')) % trainRate);
+    nextArrival = moment().add(minAway, 'minutes').format('h:mm a');
+    console.log("The next train arrives in " + minAway + " minutes at " + nextArrival);
+  };
+
+  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" +
+  trainRate + "</td><td>" + nextArrival + "</td><td>" + minAway + "</td></tr>");
+});
